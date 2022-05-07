38c7b16c2a973795c732e39d93edc00d840ab862
Haeseong Jeon <seong0428@gmail.com>
2022-05-07 13:48:47 +0900

    feat: item 목록 조회시 name 조건 추가

--- lib/adapter/api/nplusone_api.dart
+++ lib/adapter/api/nplusone_api.dart
@@ -11,16 +11,20 @@ class NplusoneApi {
   final String _host = "nplusone.hae-seong.com";
 
   Future<ApiResponse<List<ItemResponse>>> getItems({
+    String? name,
+    StoreType? storeType,
     int offsetId = 0,
     int size = 20,
-    StoreType? storeType,
   }) {
     final queryParameterMap = <String, dynamic>{};
-    queryParameterMap['offsetId'] = offsetId.toString();
-    queryParameterMap['size'] = size.toString();
+    if (name != null && name.isNotEmpty) {
+      queryParameterMap['name'] = name;
+    }
     if (storeType != null) {
       queryParameterMap['storeType'] = storeType.getQueryName();
     }
+    queryParameterMap['offsetId'] = offsetId.toString();
+    queryParameterMap['size'] = size.toString();
     return http
         .get(Uri.http(_host, '/api/v1/items', queryParameterMap))
         .then((value) => json.decode(utf8.decode(value.bodyBytes)))
