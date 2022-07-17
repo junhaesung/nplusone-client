3e2779a175497eb74b47fac4091f634c8429b31c
Haeseong Jeon <seong0428@gmail.com>
2022-07-17 12:52:41 +0900

    feat: 편의점 버튼 gridview 로 변경

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -28,33 +28,134 @@ class HomeView extends StatelessWidget {
 
   Widget _getStoreTabWidget() {
     final tabData = [
-      StoreTabData('CU', const Color.fromRGBO(245, 249, 233, 1.0)),
-      StoreTabData('GS25', const Color.fromRGBO(230, 247, 250, 1.0)),
-      StoreTabData('이마트24', const Color.fromRGBO(255, 245, 228, 1.0)),
-      StoreTabData('세븐일레븐', const Color.fromRGBO(229, 240, 230, 1.0)),
-      StoreTabData('미니스톱', const Color.fromRGBO(225, 230, 240, 1.0)),
-    ];
-    return Row(
-      children: tabData.map((e) =>
-          Expanded(
-            flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Column(
-                children: [
-                  Container(
-                    color: e.color,
-                    height: 80,
-                  ),
-                  Text(
-                    e.name,
-                    style: const TextStyle(fontSize: 14),
-                  ),
-                ],
+      StoreTabData(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'CU',
+              style: TextStyle(
+                color: Color.fromRGBO(160, 199, 59, 1.0),
+                fontSize: 14,
               ),
             ),
-          )
-      ).toList(),
+          ],
+        ),
+        color: const Color.fromRGBO(245, 249, 233, 1.0),
+      ),
+      StoreTabData(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'GS',
+              style: TextStyle(
+                color: Color.fromRGBO(14, 99, 175, 1.0),
+                fontSize: 14,
+              ),
+            ),
+            Text(
+              '25',
+              style: TextStyle(
+                color: Color.fromRGBO(42, 176, 211, 1.0),
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ),
+        color: const Color.fromRGBO(230, 247, 250, 1.0),
+      ),
+      StoreTabData(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              '이',
+              style: TextStyle(
+                fontSize: 14,
+                color: Color.fromRGBO(250, 157, 43, 1.0),
+              ),
+            ),
+            Text(
+              '마트',
+              style: TextStyle(
+                fontSize: 14,
+                color: Color.fromRGBO(62, 61, 61, 1.0),
+              ),
+            ),
+            Text(
+              '24',
+              style: TextStyle(
+                fontSize: 14,
+                color: Color.fromRGBO(250, 157, 43, 1.0),
+              ),
+            ),
+          ],
+        ),
+        color: const Color.fromRGBO(255, 245, 228, 1.0),
+      ),
+      StoreTabData(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              '세븐일레븐',
+              style: TextStyle(
+                color: Color.fromRGBO(32, 116, 41, 1.0),
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ),
+        color: const Color.fromRGBO(229, 240, 230, 1.0),
+      ),
+      StoreTabData(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              '미니스톱',
+              style: TextStyle(
+                color: Color.fromRGBO(5, 38, 118, 1.0),
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ),
+        color: const Color.fromRGBO(225, 230, 240, 1.0),
+      ),
+    ];
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        childAspectRatio: 10 / 12,
+        shrinkWrap: true,
+        children: tabData
+            .map((e) => Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Container(
+                            color: e.color,
+                          ),
+                        ),
+                      ),
+                      e.title,
+                    ],
+                  ),
+                ))
+            .toList(),
+      ),
     );
   }
 
@@ -67,16 +168,19 @@ class HomeView extends StatelessWidget {
   }
 
   Widget _getBannerWidget() {
-    return Container(
-      height: 100,
-      color: const Color.fromRGBO(222, 222, 222, 1.0),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 100,
+        color: const Color.fromRGBO(222, 222, 222, 1.0),
+      ),
     );
   }
 }
 
 class StoreTabData {
-  String name;
+  Widget title;
   Color color;
 
-  StoreTabData(this.name, this.color);
+  StoreTabData({required this.title, required this.color});
 }
