6f65815f5bad0869893e58937f6f7e39764665f3
Haeseong Jeon <seong0428@gmail.com>
2022-07-23 20:54:13 +0900

    feat: font 적용

--- lib/app.dart
+++ lib/app.dart
@@ -16,6 +16,7 @@ class NplusoneApp extends StatelessWidget {
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
+          fontFamily: 'Pretendard',
         ),
         home: const TabPageNavigator(),
       ),
